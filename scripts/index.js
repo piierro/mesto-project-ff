3e98271450cc9a72a4b511b2132a61ba1e69c336
Kira Murzaneva <marccappy76@gmail.com>
2023-12-20 17:08:39 +0300

    card

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,18 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardContainer = document.querySelector('.places__list');
+
+initialCards.forEach(function (element) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__title').textContent = element.name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function () {cardElement.remove()});
+
+    cardContainer.append(cardElement);
+});
